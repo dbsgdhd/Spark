2819ad8b45e8c23a4c7ac801893ae401618728b0
babybattle <2816512220@qq.com>
2021-01-02 15:29:16 +0800

    spark02

--- src/main/java/com/dingqi/spark/TestRdd.java
+++ src/main/java/com/dingqi/spark/TestRdd.java
@@ -3,6 +3,7 @@ package com.dingqi.spark;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.rdd.JdbcRDD;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,6 +12,7 @@ public class TestRdd {
     public static void main(String[] args) {
         SparkConf rdd = new SparkConf().setMaster("local[*]").setAppName("WordCount");
         JavaSparkContext sc = new JavaSparkContext(rdd);
+        JdbcRDD<Object> objectJdbcRDD = new JdbcRDD<>();
 //        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
 //        JavaRDD<Integer> parallelize = sc.parallelize(list,2);
         JavaRDD<String> parallelize = sc.textFile("in",2);
